d0e2404a444f43f453f769fe5bb70d1fd68edd99
Vishal Sharma <vsh1819@gmail.com>
2018-04-11 15:52:25 +0100

    created the addnote function to add notes to the json file

---
+++ notes.js
@@ -0,0 +1,47 @@
+console.log('Starting notes.js');
+
+const fs = require('fs')
+
+
+let addNote = (title, body) => {
+  let notes = []
+  let note = {
+    title,
+    body
+  }
+
+  try {
+    let notesString = fs.readFileSync('notes-data.json')
+    notes = JSON.parse(notesString)
+  } catch (e) {
+
+  }
+
+  let duplicateNotes = notes.filter((note) => note.title === title)
+
+  if (duplicateNotes.length === 0 ) {
+    notes.push(note)
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes))
+  }
+
+
+}
+
+let getAll = () => {
+  console.log('getting all notes');
+}
+
+let getNote = (title) => {
+  console.log('Getting note', title);
+}
+
+let removeNote = (title) => {
+  console.log('Removing note', title);
+}
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+}
