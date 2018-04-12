adf1f416f7269e92067ce4ed1021b22c0bb9124b
Vishal Sharma <vsh1819@gmail.com>
2018-04-12 08:48:11 +0100

    created the removenote function and updated the console log message when a note is removed

--- notes.js
+++ notes.js
@@ -44,7 +44,11 @@ let getNote = (title) => {
 }
 
 let removeNote = (title) => {
-  console.log('Removing note', title);
+  let notes = fetchNotes()
+  let filteredNotes = notes.filter((note) => note.title !== title)
+  saveNotes(filteredNotes)
+
+  return notes.length !== filteredNotes.length
 }
 
 module.exports = {
